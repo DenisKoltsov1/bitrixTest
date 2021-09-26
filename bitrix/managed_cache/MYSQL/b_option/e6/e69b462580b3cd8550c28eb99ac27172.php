5fd00164dc098b909d209130c4d7b379b79f4115
DenisKoltsov1 <dublin87@mail.ru>
2021-09-26 16:38:59 +0300

    init srart-task

---
+++ bitrix/managed_cache/MYSQL/b_option/e6/e69b462580b3cd8550c28eb99ac27172.php
@@ -0,0 +1,7 @@
+<?
+if($INCLUDE_FROM_CACHE!='Y')return false;
+$datecreate = '001632659660';
+$dateexpire = '001632663260';
+$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:1:{s:1:"-";a:2:{s:20:"ipcheck_disable_file";s:64:"/bitrix/modules/ipcheck_disable_6e2a4cccf642179cf039255763bba23a";s:12:"redirect_sid";s:32:"lpblqov0wye20cwta4lq452i3kp0al1w";}}}';
+return true;
+?>
---
+++ bitrix/managed_cache/MYSQL/js/s1/eshop_bootstrap_v4/template_963c73ce225de467a7977759034d6e68/info_v1.php
@@ -0,0 +1,1 @@
+<?php $filesInfo = ["/bitrix/components/bitrix/sale.basket.basket.line/templates/bootstrap_v4/script.js" => "16326593893841","/bitrix/components/bitrix/menu/templates/bootstrap_v4/script.js" => "16326591154044","/bitrix/components/bitrix/search.title/script.js" => "16326594076443","/bitrix/components/bitrix/news.list/templates/bootstrap_v4/script.js" => "16326592623253",]; ?>
